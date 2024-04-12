65a56506760796e0979b845c56062cf0b9615ac2
Yaoyao Hu <shady030314@gmail.com>
2024-04-12 16:16:08 +0800

    lab02 finished

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -1,27 +1,32 @@
-#include <stdio.h>
 #include "bit_ops.h"
+
+#include <stdio.h>
+
+/// ONLY use (&), or (|), xor (^), not (~), left shifts («), and right shifts
+/// (»). You may not use any for/while loops or conditional statements. You also
+/// may not use modulo (%), division, addition subtraction, or multiplication.
 
 // Return the nth bit of x.
 // Assume 0 <= n <= 31
-unsigned get_bit(unsigned x,
-                 unsigned n) {
-    // YOUR CODE HERE
-    // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns 
-    // 0 or 1)
-    return -1;
-}
-// Set the nth bit of the value of x to v.
-// Assume 0 <= n <= 31, and v is 0 or 1
-void set_bit(unsigned * x,
-             unsigned n,
-             unsigned v) {
-    // YOUR CODE HERE
-}
-// Flip the nth bit of the value of x.
-// Assume 0 <= n <= 31
-void flip_bit(unsigned * x,
-              unsigned n) {
-    // YOUR CODE HERE
+unsigned get_bit(unsigned x, unsigned n) {
+  //   0b0000100 <- (0b0000001 << 2)
+  // & 0b1001110
+  //   0b0000100 >> 2 -> 0b0000001
+  return (x & (1 << n)) >> n;
 }
 
+// Set the nth bit of the value of x to v.
+// Assume 0 <= n <= 31, and v is 0 or 1
+void set_bit(unsigned* x, unsigned n, unsigned v) {
+  if (get_bit(*x, n) == v) return;
+  *x = *x ^ (1 << n);
+}
+
+// Flip the nth bit of the value of x.
+// Assume 0 <= n <= 31
+void flip_bit(unsigned* x, unsigned n) {
+  if (get_bit(*x, n) == 0b1)
+    set_bit(x, n, 0);
+  else
+    set_bit(x, n, 1);
+}
--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -1,10 +1,20 @@
-#include <stdio.h>
+// clang-format off
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "lfsr.h"
+#include "bit_ops.h" // rules are meant to break...
+// clang-format on
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
-}
+  unsigned bit_0 = get_bit(*reg, 0);
+  unsigned bit_2 = get_bit(*reg, 2);
+  unsigned bit_3 = get_bit(*reg, 3);
+  unsigned bit_5 = get_bit(*reg, 5);
+  unsigned bit = ((bit_0 ^ bit_2) ^ bit_3) ^ bit_5;
 
+  *reg = *reg >> 1;
+  set_bit((unsigned *)reg, 15, bit);
+}
